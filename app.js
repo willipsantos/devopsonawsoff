2f2170efa3a22e4eaf6d9aad00dd9d603a10e704
Ubuntu <ubuntu@ip-172-31-61-186.ec2.internal>
2021-03-20 11:58:04 +0000

    change message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello World! com passo manual')
+  res.send('Hello World! RM 337745')
 })
 
 app.listen(port, () => {
